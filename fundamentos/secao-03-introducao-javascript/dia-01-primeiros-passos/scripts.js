72bb99617bfd82a2f448e9fd7f158632d36ed934
Paula Pinheiro <paulapinheirogyn@hotmail.com>
2023-05-01 18:20:37 -0300

    exercicio 9

--- fundamentos/secao-03-introducao-javascript/dia-01-primeiros-passos/scripts.js
+++ fundamentos/secao-03-introducao-javascript/dia-01-primeiros-passos/scripts.js
@@ -1,8 +1,8 @@
 let numero1 = 6;
 let numero2 = 3;
-let numero3 = 5;
+let numero3 = 2;
 
-if (numero1 % 2 === 0 || numero2 % 2 === 0 || numero3 % 2 === 0) {
+if (numero1 % 2 !== 0 || numero2 % 2 !== 0 || numero3 % 2 !== 0) {
     console.log (true)
 } else {
     console.log (false);
