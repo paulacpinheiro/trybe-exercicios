27393174cb7778f6e3fe1be4b26e8db9d1c8d677
Paula Pinheiro <paulapinheirogyn@hotmail.com>
2023-05-01 18:03:28 -0300

    exercicio 7

--- fundamentos/secao-03-introducao-javascript/dia-01-primeiros-passos/scripts.js
+++ fundamentos/secao-03-introducao-javascript/dia-01-primeiros-passos/scripts.js
@@ -1,24 +1,22 @@
-let peca = "rei";
+let porcentagem = 59;
 
-switch (peca.toLowerCase()) {
-    case "peão":
-        console.log("uma casa para frente, na primeira jogada pode andar duas");
-        break;
-    case "cavalo":
-        console.log("duas casas para frente e uma casa para o lado esquerdo ou o lado direito");
-        break;
-    case "torre":
-        console.log("movimento horizontal ou vertical");
-        break;
-    case "rei":
-        console.log("uma casa em qualquer direção");
-        break;
-    case "rainha":
-        console.log("diagonal, vertical e horizontal");
-        break;
-    case "bispo":
-        console.log("diagonal");
-        break;
-    default:
-        console.log("Erro.Peça inválida");
+if (porcentagem < 0 || porcentagem > 100){
+    console.log("ERRO");
+} else if (porcentagem >= 90) {
+    console.log("A");
+}
+else if (porcentagem >= 80) {
+    console.log("B");
+}
+else if (porcentagem >= 70) {
+    console.log("C");
+}
+else if (porcentagem >= 60) {
+    console.log("D");
+}
+else if (porcentagem >= 50) {
+    console.log("E");
+}
+else {
+    console.log("F");
 };
