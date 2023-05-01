22f74d5ea22e105f607838db77b415a4b7469e85
Paula Pinheiro <paulapinheirogyn@hotmail.com>
2023-05-01 16:29:00 -0300

    exercicio 1

--- fundamentos/secao-03-introducao-javascript/dia-01-primeiros-passos/scripts.js
+++ fundamentos/secao-03-introducao-javascript/dia-01-primeiros-passos/scripts.js
@@ -1,16 +1,8 @@
-let situacao = "aprovada";
+let a = 8;
+let b = 3;
 
-switch (situacao) {
-    case "aprovada":
-        console.log("Parabéns, você está no grupo de pessoas!");
-        break;
-    case "lista":
-        console.log("VOcê está na lista de espera");
-        break;
-    case "reprovada":
-        console.log("Infelizmente, você reprovou");
-        break;
-    default:
-        console.log("Informação incorreta");
-
-}
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
