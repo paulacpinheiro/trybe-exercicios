6a824d0e94d478e491b9907a2a2532501db7bfd6
Paula Pinheiro <paulapinheirogyn@hotmail.com>
2023-05-01 16:43:55 -0300

    exercicio 3

--- fundamentos/secao-03-introducao-javascript/dia-01-primeiros-passos/scripts.js
+++ fundamentos/secao-03-introducao-javascript/dia-01-primeiros-passos/scripts.js
@@ -1,8 +1,15 @@
 let a = 1;
 let b = 3;
+let c = 6;
 
-if (a > b){
-    console.log("'a' é maior que 'b'");
-} else {
-    console.log("'b' é maior que 'a'");
+
+if(a > b && a > c) {
+    console.log("'a' é o maior número");
 }
+else if (b > a && b > c) {
+    console.log("'b' é o maior número");
+}
+else {
+    console.log("'c' é o maior número");
+}
+
