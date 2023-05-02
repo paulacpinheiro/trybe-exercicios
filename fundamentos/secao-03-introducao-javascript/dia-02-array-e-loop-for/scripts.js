b8ea0519088452ead5656d49df628d199eaa79c5
Paula Pinheiro <paulapinheirogyn@hotmail.com>
2023-05-02 19:05:49 -0300

    exercicio sobre for

--- fundamentos/secao-03-introducao-javascript/dia-02-array-e-loop-for/scripts.js
+++ fundamentos/secao-03-introducao-javascript/dia-02-array-e-loop-for/scripts.js
@@ -1,11 +1,5 @@
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = menu[1];
-//console.log(menuServices);//
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 
-
-
-let indexOfPortfolio = menu.indexOf('Portfólio');
-    //console.log(indexOfPortfolio);//
-
-   /* menu.push("Contato");
-    console.log(menu);*/
+for (let index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]);
+};
