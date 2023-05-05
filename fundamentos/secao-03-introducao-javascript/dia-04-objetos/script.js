99ace9c3dd14a0366523830f038cdc59a126ba0a
Paula Pinheiro <paulapinheirogyn@hotmail.com>
2023-05-05 09:45:59 -0300

    exercicios objetos e for/in

--- fundamentos/secao-03-introducao-javascript/dia-04-objetos/script.js
+++ fundamentos/secao-03-introducao-javascript/dia-04-objetos/script.js
@@ -1,35 +1,26 @@
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+        {
+            titulo: 'O Pior Dia de Todos',
+            autor: 'Daniela Kopsch',
+            editora: 'Tordesilhas',
+        },
+    ],
 };
 
+leitor.livrosFavoritos.push(
+    {
+        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+        autor: 'JK Rowling',
+        editora: 'Rocco',
 
-console.log('Bem vinda, ' + info.personagem)
+    },
+);
 
-info['recorrente'] = 'Sim';
-console.log(info);
+for (let key in leitor) {
+    console.log(leitor.nome +  ' tem ' + leitor.livrosFavoritos.length  + ' livros favoritos')
 
-
-for (let key in info) {
-    console.log(key);
-}
-
-for (let key in info) {
-    console.log(info[key]);
-}
-
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim',
-};
-
-for (let key in info) {
-    if (key === 'recorrentes' && info[key] === 'sim' && info2[key] === 'sim') {
-        console.log('Todos recorrentes');
-    } else {
-        console.log(info[key] + ' e ' + info2[key]);
-    }
 }
