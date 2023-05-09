bc33abb50abbca75ff63fec5ee64a4a30e12ecdd
Paula Pinheiro <paulapinheirogyn@hotmail.com>
2023-05-08 21:23:40 -0300

    exercicio bonus

--- fundamentos/secao-03-introducao-javascript/dia-05-javascript-es6/script.js
+++ fundamentos/secao-03-introducao-javascript/dia-05-javascript-es6/script.js
@@ -1,51 +1,27 @@
-function imprimeIdade() {
-    for (let idade = 30; idade <= 40; idade += 1) {
-        //console.log('Idade dentro do for:', idade)
+//Função 1
+const substituaX = (person) => {
+    const frase = 'Trybe x aqui!';
+    const fraseArray = frase.split(' ');
+    for (let index = 0; index < fraseArray.length; index += 1) {
+        if (fraseArray[index] === 'x') {
+            fraseArray[index] = person;
+        }
     }
+    return fraseArray.join(' ');
+};
+//console.log(substituaX('Bebeto'));
 
-}
-imprimeIdade()
+//Função 2
+const minhasSkills = (substituaX) => {
+    const skills = ['HTML', 'CSS', 'JavaScript'];
+    let result = `${substituaX} 
 
-
-const pessoa = {
-    nome: 'Henri',
-    idade: 20
-}
-pessoa.nome = 'Luna';
-pessoa.idade = 19;
-
-//console.log('Nome:', pessoa.nome);
-//console.log('Idade:', pessoa.idade);
-
-let favoriteFood = 'Lasanha';
-favoriteFood = 'Hambúrguer';
-//console.log(favoriteFood);
-
-const name = 'Adriana';
-const lastName = 'Soares';
-//console.log(`Olá, ${name} ${lastName}!`);
-function soma(a, b) {
-    let resultado = a + b;
-    return resultado;
-}
-let a = 3;
-let b = 5;
-//console.log(`O resultado da soma de ${a} + ${b} é: ${a + b}`);
-
-const numeroAleatorio = () => Math.random();
-//console.log(numeroAleatorio());
-
-const hello = nome => `Olá, ${nome}!`;
-//let nome = 'Ivan';
-//console.log(hello(nome));
-
-const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome}`;
-let nome = 'Ivan';
-let sobrenome = 'Pires';
-//console.log(nomeCompleto(nome, sobrenome));
-
-
-let speed = 130;
-const speedCar = (speed >= 120) ? 'Você ultrapassou o limite de velocidade' : 'Você está na velocidade permitida';
-
-console.log(speedCar);
+    Minhas principais habilidades são:
+    `;
+    for (let index = 0; index < skills.length; index += 1) {
+        result = `${result}
+        - ${skills[index]}`;
+    }
+    return result;
+};
+console.log(minhasSkills(substituaX('Bebeto')));
